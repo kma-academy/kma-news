31c13c25fe4b1187b514005e1dc478806dc0a81f
Trần Đức Cường <clonebmn2itt@gmail.com>
2022-03-10 14:58:03 +0700

    fix env validation schema

--- libs/env-validation-schema/src/index.ts
+++ libs/env-validation-schema/src/index.ts
@@ -1,24 +1,14 @@
 import Joi from 'joi';
+import { databaseValidation } from './database';
+import { elasticSearchValidateSchema } from './elasticsearch';
+import { jwtValidation } from './jwt';
 export const EnvValidationSchema = Joi.object({
   NODE_ENV: Joi.string()
     .valid('development', 'production', 'test', 'provision')
     .default('development'),
   PORT: Joi.number().default(3000),
-  DATABASE_HOST: Joi.string().required(),
-  DATABASE_PORT: Joi.number().required(),
-  DATABASE_USERNAME: Joi.string().required(),
-  DATABASE_PASSWORD: Joi.string().allow('').required(),
-  DATABASE_NAME: Joi.string().required(),
-  ACCESS_TOKEN_SECRET: Joi.string().required(),
-  ACCESS_TOKEN_TTL: Joi.number()
-    .min(1)
-    .max(10 * 60),
-  REFRESH_TOKEN_SECRET: Joi.string()
-    .invalid(Joi.ref('ACCESS_TOKEN_SECRET'))
-    .required(),
-  REFRESH_TOKEN_TTL: Joi.number()
-    .min(24 * 60 * 60)
-    .required(),
+  ...databaseValidation,
+  ...jwtValidation,
   ZALO_SECRET_KEY: Joi.string().required(),
   ZALO_APP_ID: Joi.string().required(),
   COOKIE_DOMAIN: Joi.string().required(),
@@ -28,4 +18,5 @@ export const EnvValidationSchema = Joi.object({
   ADMIN_HOST: Joi.string().required(),
   FRONTEND_HOST: Joi.string().required(),
   BACKEND_HOST: Joi.string().required(),
+  ...elasticSearchValidateSchema
 });
