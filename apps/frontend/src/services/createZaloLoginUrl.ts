20749f37325e27a2f17d9d7052e8744a3f367439
Trần Đức Cường <36887165+Notekunn@users.noreply.github.com>
2022-03-11 15:48:00 +0700

    fix deploy to vps (#64)
    * fix: zalo default parameter
    
    * fix not change topic title when switch topics
    
    * fix crawler command

--- apps/frontend/src/services/createZaloLoginUrl.ts
+++ apps/frontend/src/services/createZaloLoginUrl.ts
@@ -1,3 +1,7 @@
+const DEFAULT_APP_ID = '513056836020745485';
+const DEFAULT_CALLBACK_URL = 'https://blog.kma-news.tech/auth/login/zalo';
 export default function createZaloLoginUrl(appId: string, callbackURL: string) {
-  return `https://oauth.zaloapp.com/v4/permission?app_id=${appId}&redirect_uri=${callbackURL}&state=react-client`
+  return `https://oauth.zaloapp.com/v4/permission?app_id=${
+    appId || DEFAULT_APP_ID
+  }&redirect_uri=${callbackURL || DEFAULT_CALLBACK_URL}&state=react-client`;
 }
